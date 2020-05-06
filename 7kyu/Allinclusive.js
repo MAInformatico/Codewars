971da4a4f33d4d0e267b012523140d21345afe46
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-05-06 07:23:20 +0200

    Create Allinclusive.js

---
+++ 7kyu/Allinclusive.js
@@ -0,0 +1,6 @@
+function containAllRots(strng, arr) {
+  for (let i = 0; i < strng.length; i++)
+      if (arr.indexOf(strng.slice(i) + strng.slice(0, i)) === -1)
+        return false;
+    return true;
+}
