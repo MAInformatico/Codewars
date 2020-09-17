095bf315209678a99418bc5dd606cd6ae53a78a9
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-09-17 06:36:15 +0200

    Create BreakcamelCase.js

---
+++ 6kyu/BreakcamelCase.js
@@ -0,0 +1,4 @@
+// complete the function
+function solution(string) {
+  return(string.replace(/([A-Z])/g, ' $1')); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/n
+}
