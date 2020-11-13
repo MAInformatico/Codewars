e2983ea68b4168c9ac2586e5a941e54324d7a053
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-11-13 08:17:15 +0100

    Update arithmetic.c

--- 7kyu/arithmetic.c
+++ 7kyu/arithmetic.c
@@ -1,7 +1,7 @@
 #include <string.h>
 
 double arithmetic(double a, double b, char* operator) {
-  float result = 0;
+  double result = 0;
   if(strcmp(operator, "add") == 0 ) result = a + b;
   if(strcmp(operator,"subtract") == 0) result = a - b;
   if(strcmp(operator, "multiply") == 0) result = a * b;
