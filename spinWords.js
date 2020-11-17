e748b1b6f2336cb8455416006435b4a8dbe4ec16
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-11-17 07:51:09 +0100

    Delete spinWords.js

--- spinWords.js
+++
@@ -1,7 +0,0 @@
-  const spinWords = str => {
-    const array = str.split(" ");
-    const reversearray = array.map(word => {
-       return word.length >= 5 ? word.split("").reverse().join("") : word;
-    })
-  return reversearray.join(" ");
-}
