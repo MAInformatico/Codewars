2ee9ad6245456184c8ab6372d16b62495bcc0845
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2021-01-03 11:54:05 +0100

    Create irreducibleSumRationals.c

---
+++ 6kyu/irreducibleSumRationals.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+long long mcm(long long a, long long b){
+  long long common = b;
+  while(a%b!=0){
+    common = a%b;
+    a = b;
+    b = common;
+  }
+  return common;
+}
+
+// row: number of rows of lst
+int* sumFracts(int lst[][2], int row) {
+    int *result = (int*)calloc(2,sizeof(int));
+    result[1] = 1;
+    if (row == 0) return result;
+    long long sumDenominators = 1;
+    long long sumNumerators = 0;
+    for(int i = 0; i<row;i++) sumDenominators*=lst[i][1];
+    for(int i = 0; i<row;i++) sumNumerators+=sumDenominators*lst[i][0]/lst[i][1];
+    long long commonDenominator = mcm(sumNumerators,sumDenominators);
+    result[0] =sumNumerators/commonDenominator;
+    result[1] =sumDenominators/commonDenominator;
+    return result;
+}
