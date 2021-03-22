75ccc6339f8375724a72020744f70f8eb1b10f6c
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2021-03-22 06:29:06 +0100

    Create Multiplesof3or5.js

---
+++ 6kyu/Multiplesof3or5.js
@@ -0,0 +1,7 @@
+function solution(number){
+  let result = 0;
+    for(let i = 0; i<number;i++){
+      if((i%5==0 && i%3!=0) || (i%5!=0 && i%3==0) || (i%5==0 && i%3==0) ) result+=i;
+    }
+    return result;
+}
